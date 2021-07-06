a68a1ea7d96ecf4798ac599293383284cd68a846
Karunesh Tripathi <karunesh.tripathi2019@vitstudent.ac.in>
2021-07-07 03:21:36 +0530

    deployment changes

--- server.js
+++ server.js
@@ -52,4 +52,10 @@ app.post('/api/forma',(req,res)=>{
     })
     smtptransport.close()
 })
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,"client",  "build", "index.html"));
+    });
+  }
 app.listen( PORT,()=>console.log(`Server Started...${PORT}`));
